d26394e0b5e1b23afcb02fd4334d010b41d14d73
kassimii <patyboc@yahoo.com>
2020-12-17 23:11:40 +0200

    Created admin screens for orders&products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,9 +3,17 @@ const router = express.Router();
 import {
   getProductById,
   getProducts,
+  deleteProduct,
+  updateProduct,
+  createProduct,
 } from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
-router.get('/', getProducts);
-router.get('/:id', getProductById);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 export default router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,9 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
+import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
+import OrderListScreen from './screens/OrderListScreen';
 
 function App() {
   return (
@@ -23,6 +26,9 @@ function App() {
       <Header />
       <main className='py-3'>
         <Container>
+          <Route path='/admin/orderlist' component={OrderListScreen} />
+          <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
+          <Route path='/admin/productlist' component={ProductListScreen} />
           <Route path='/admin/userlist' component={UserListScreen} />
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
